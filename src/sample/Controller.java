c6ea0c244e09e902194ec22aceb301e5be06c284
fritzChristoph <fritzc@edu.htl-villach.at>
2021-06-28 14:47:59 +0200

    first webviwer

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -89,7 +89,7 @@ public class Controller implements Initializable {
                 Scene scene = null;
                 root = loader.load();
                 controller = loader.getController();
-                //controller.setCity(currentCity);
+                controller.setCity(currentCity);
                 stage = new Stage();
                 scene = new Scene(root);
                 stage.setTitle("Projectwork");
@@ -116,7 +116,7 @@ public class Controller implements Initializable {
                 Scene scene = null;
                 root = loader.load();
                 controller = loader.getController();
-                //controller.setCity(currentCity);
+               // controller.setCity(currentCity);
                 stage = new Stage();
                 scene = new Scene(root);
                 stage.setTitle("Projectwork");
--- src/sample/ControllerWebside.java
+++ src/sample/ControllerWebside.java
@@ -1,8 +1,44 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sample.bll.City;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ControllerWebside {
-    public void onclickWVback(ActionEvent actionEvent) {
+
+    private City cityVi;
+
+    @FXML
+    private Button closeButton;
+    private String urlofWeb;
+    @FXML
+    private WebView idWebview;
+
+
+    public void setCity(City currentCity) {
+        cityVi = currentCity;
+        urlofWeb = cityVi.getWebseite();
+        //idWebview.getEngine().load(urlofWeb);
     }
+
+    public void onclickWVback(ActionEvent actionEvent)  {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+
 }
